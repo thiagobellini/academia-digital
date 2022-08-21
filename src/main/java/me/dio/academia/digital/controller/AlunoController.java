d9dc1d00b5e130992213f18284bf3cb8c92bc0b2
Thiago Bellini <thiagobellinics@hotmail.com>
2022-08-21 17:40:06 -0300

    Alterações de JPA

--- src/main/java/me/dio/academia/digital/controller/AlunoController.java
+++ src/main/java/me/dio/academia/digital/controller/AlunoController.java
@@ -1,9 +1,34 @@
 package me.dio.academia.digital.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import me.dio.academia.digital.entity.Aluno;
+import me.dio.academia.digital.entity.AvaliacaoFisica;
+import me.dio.academia.digital.entity.form.AlunoForm;
+import me.dio.academia.digital.service.impl.AlunoServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 @RequestMapping("/alunos")
 public class AlunoController {
+
+    @Autowired
+    private AlunoServiceImpl service;
+
+    @GetMapping
+    public List<Aluno> getAll(@RequestParam(value = "dataDeNascimento", required = false) String dataDeNascimento) {
+        return service.getAll(dataDeNascimento);
+    }
+
+    @PostMapping
+    public Aluno create(@Valid @RequestBody AlunoForm alunoForm) {
+        return service.create(alunoForm);
+    }
+
+    @GetMapping("/avaliacoes/{id}")
+    public List<AvaliacaoFisica> getAllAvalicaoFisicaById(@PathVariable Long id) {
+        return service.getAllAvaliacaoFisica(id);
+    }
 }
